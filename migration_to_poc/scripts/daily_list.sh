41d18090dae3444aa87dd23cf4f264a92e8324b7
amosh101 <amosh101@outlook.com>
2019-10-23 14:39:18 +0200

    To run daily

--- migration_to_poc/scripts/daily_list.sh
+++ migration_to_poc/scripts/daily_list.sh
@@ -1,7 +1,7 @@
 echo 'daily list script starting';
 cd /opt/sesp-repository/
 
-mysql -uroot -ppassword -D openmrs -e "select a.identifier as NID, b.given_name as nome, b.family_name as apelido,date(c.encounter_datetime) as data_visita from patient_identifier a, person_name b, encounter c where a.patient_id = b.person_id and b.person_id = c.patient_id and a.identifier_type = 3;" > list.txt
+mysql -uroot -ppassword -D openmrs -e "select a.identifier as NID, b.given_name as nome, b.family_name as apelido,date(c.encounter_datetime) as data_visita from patient_identifier a, person_name b, encounter c where a.patient_id = b.person_id and b.person_id = c.patient_id and a.identifier_type = 3 and date(c.encounter_datetime) = current_date();" > list.txt
 
 exit
 echo 'daily list script finished';
